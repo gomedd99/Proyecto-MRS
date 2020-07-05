15842fd7c453f47897a44c4e24b3405e0ea5366f
gomedd99 <gomedd99@aol.com>
2020-07-05 16:49:47 -0500

    Productos

---
+++ Inventario/Producto.java
@@ -0,0 +1,60 @@
+import java.io.Serializable;
+public class Producto implements Serializable {
+    private String Nombre;
+    private int id;
+    private int Existensias;
+
+	public Producto(String Nombre, int id, int Existensias) {
+		this.Nombre = Nombre;
+		this.id = id;
+		this.Existensias = Existensias;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public Boolean setNombre(String Nombre) {
+        try {
+            this.Nombre = Nombre;
+        } catch(Exception e) {
+            return false;
+        }
+        return true;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Boolean setId(int id) {
+        try {
+            this.id = id;
+        } catch(Exception e) {
+            System.out.println("no");
+            return false;
+        }
+        return true;
+	}
+
+	public int getExistensias() {
+		return Existensias;
+	}
+
+	public Boolean setExistensias(int Existensias) {
+        try {
+            this.Existensias = Existensias;
+        } catch(Exception e) {
+            return false;
+        }
+        return true;
+	}
+    public Boolean agregarExistencias(int masExistensias){
+        try {
+            Existensias += masExistensias;
+        } catch(Exception e) {
+            return  false;
+        }
+        return true;
+    }
+}
