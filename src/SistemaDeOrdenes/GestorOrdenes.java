4c1b977fa0899e3adc83dfa4d450d72e5155752e
tan(x) <taniazuniga.mx@gmail.com>
2020-07-09 16:17:29 -0500

    Create GestorOrdenes.java
    Cada metodo de esta clase recibe un Pago y un Carrito. (menos verOrdenes)
    
    Se agrego el metodo verOrdenes

---
+++ src/SistemaDeOrdenes/GestorOrdenes.java
@@ -0,0 +1,46 @@
+package SistemaDeOrdenes;
+
+import SistemaDeCarrito.Carrito;
+import SistemaDePago.Pago;
+
+public class GestorOrdenes {
+
+    private ListaOrdenes listaOrdenes;
+
+    public GestorOrdenes(ListaOrdenes listaOrdenes) {
+        this.listaOrdenes = listaOrdenes;
+    }
+
+    public Orden generarOrden(Pago pago, Carrito carrito){
+
+        Orden nuevaOrden = new Orden(pago, carrito);
+
+        return nuevaOrden;
+    }
+
+    public boolean agregarOrden(Pago pago, Carrito carrito){
+        return listaOrdenes.agregarOrden( generarOrden(pago, carrito) );
+    }
+
+    public boolean eliminarOrden(int id){
+        return listaOrdenes.borrarOrden(id);
+    }
+
+    public boolean editarOrden(Pago pago, Carrito carrito, int id) {
+        boolean regresar = false;
+        Orden aEditar = listaOrdenes.getOrden(id);
+
+        if (aEditar != null) {
+            listaOrdenes.borrarOrden(id);
+            aEditar.setCarrito(carrito);
+            aEditar.setPago(pago);
+            regresar = listaOrdenes.agregarOrden(aEditar);
+        }
+        return regresar;
+    }
+
+    public String verOrdenes(){
+        return listaOrdenes.toString();
+    }
+
+}
