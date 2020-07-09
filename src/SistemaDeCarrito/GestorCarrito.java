51e9d7e3f7e6649c9d86eedf8233771057f4aa14
pandyshwallee <andreaar1605@gmail.com>
2020-07-09 17:36:40 -0500

    Edit GestorCarrito
    Se agregaron comentarios

--- src/SistemaDeCarrito/GestorCarrito.java
+++ src/SistemaDeCarrito/GestorCarrito.java
@@ -14,12 +14,14 @@ public class GestorCarrito {
     }
 
     public Carrito generarCarrito(ArrayList<Platillo> platillos){
-        // crea un carrito nuevo
+        // Crea un carrito nuevo en this.carrito
+        // Se puede usar para vaciar el carrito
         carrito = new Carrito(platillos);
         return carrito;
     }
 
     public boolean agregarPatillos(ArrayList<Platillo> platillos) {
+        // Agrega una lista de platillos al carrito
         boolean regresar = true;
         for (int i=0;i<platillos.size();i++) {
             Platillo p = platillos.get(i);
@@ -28,15 +30,23 @@ public class GestorCarrito {
         return regresar;
     }
 
+    public boolean agregarPatillo(Platillo platillo) {
+        // Agrega un solo platillo al carrito
+        return carrito.agregarPlatillos(platillo);
+    }
+
     public boolean eliminarPatillo(int id) {
+        // Borra un platillo con "id"
         return carrito.borrarPlatillo(id);
     }
 
     public float extraerCuenta(){
+        // Regresa la cuenta $$ del carrito
         return carrito.getCuenta();
     }
 
     public Carrito getCarrito() {
+        // Regresa el carrito actual
         return carrito;
     }
 
