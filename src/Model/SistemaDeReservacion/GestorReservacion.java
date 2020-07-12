95f37a798a0bfa4146121306c9dc6204c8fbc81b
tan(x) <taniazuniga.mx@gmail.com>
2020-07-12 15:53:36 -0500

    Update GestorReservacion.java
    Los metodos del GestorReservacion reciben una fecha de tipo Calendar

--- src/Model/SistemaDeReservacion/GestorReservacion.java
+++ src/Model/SistemaDeReservacion/GestorReservacion.java
@@ -2,22 +2,20 @@ package Model.SistemaDeReservacion;
 
 import Model.SistemaDeAutenticacion.Usuario;
 
+import java.io.IOException;
 import java.util.Calendar;
 
 public class GestorReservacion {
 
     private AgendaRestaurante agendaRestaurante;
 
-    public GestorReservacion(AgendaRestaurante agendaRestaurante) {
-        this.agendaRestaurante = agendaRestaurante;
+    public GestorReservacion() throws IOException {
+        agendaRestaurante = new AgendaRestaurante();
     }
 
-    public Reservacion crearReservacion(int año, int mes, int dia, int hora, int min, Usuario cliente){
+    public Reservacion crearReservacion(Calendar aReservar , Usuario cliente){
         // Regresa null cuando no es posible hacer la reservacion, porque el dia no es valido
         Reservacion nueva = null;
-
-        Calendar aReservar = Calendar.getInstance();
-        aReservar.set(año, mes, dia, hora, min, 0);
 
         Calendar hoy = Calendar.getInstance();
 
@@ -44,11 +42,9 @@ public class GestorReservacion {
         return regresar;
     }
 
-    public Reservacion verReservacion(int año, int mes, int dia, int hora, int min, String usuario){
+    public Reservacion verReservacion(Calendar aBuscar, String usuario){
         // Si encuentra la reservacion con dicha fecha y a nombre del usuario, la regresa
         // sino regresa null
-        Calendar aBuscar = Calendar.getInstance();
-        aBuscar.set(año, mes, dia, hora, min, 0);
 
         Reservacion regresar = null;
         Reservacion buscada = agendaRestaurante.buscarReservacion(aBuscar, usuario);
@@ -71,14 +67,12 @@ public class GestorReservacion {
         return regresar;
     }
 
-    public boolean eliminarReservacion( int año, int mes, int dia, int hora, int min, String usuario){
+    public boolean eliminarReservacion( Calendar fechaPaEliminar , String usuario){
         // Borra la reservacion de la agenda que coincida con los datos dados
         // si no encuentra una reservacion con dicho id regresa null
         boolean regresar = false;
-        Calendar fecha_a_Eliminar = Calendar.getInstance();
-        fecha_a_Eliminar.set(año, mes, dia, hora, min, 0);
 
-        Reservacion aEliminar = agendaRestaurante.buscarReservacion(fecha_a_Eliminar, usuario);
+        Reservacion aEliminar = agendaRestaurante.buscarReservacion(fechaPaEliminar, usuario);
 
         if ( aEliminar != null){
             if ( agendaRestaurante.borrarReservacion(aEliminar) ){
@@ -88,24 +82,19 @@ public class GestorReservacion {
         return regresar;
     }
 
-    public boolean modificarReservacion(int año, int mes, int dia, int hora, int min, int id){
+    public Reservacion modificarReservacion(Calendar nuevaFecha, int id){
         // modifica la reservacion con "id" por la fecha dada.
         // regresa true si se hizo el cambio, sino regresa false
-        boolean regresar = false;
 
         Reservacion aModificar = agendaRestaurante.buscarReservacion(id);
         agendaRestaurante.borrarReservacion(aModificar);
 
-        Calendar nuevaFecha = Calendar.getInstance();
-        nuevaFecha.set(año, mes, dia, hora, min, 0);
-
         // si la fecha es valida entonces se agrega a la agenda
         if ( aModificar.setFecha(nuevaFecha) ){
-            regresar = agendaRestaurante.agregarReservacion(aModificar);
+            agendaRestaurante.agregarReservacion(aModificar);
+            return aModificar;
         }
-
-        return regresar;
-
+        return null;
     }
 
     public String verReservaciones(){
