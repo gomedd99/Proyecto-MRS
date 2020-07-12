e0f4a0b7fe27d52a3bab071aa2d7424ee9405499
tan(x) <taniazuniga.mx@gmail.com>
2020-07-12 18:22:27 -0500

    Update GestorOrdenes.java
    se agrego "autoguardado" de ListaOrdenes en "ListaOrdenes.bin"

--- src/Model/SistemaDeOrdenes/GestorOrdenes.java
+++ src/Model/SistemaDeOrdenes/GestorOrdenes.java
@@ -3,12 +3,14 @@ package Model.SistemaDeOrdenes;
 import Model.SistemaDeCarrito.Carrito;
 import Model.SistemaDePago.Pago;
 
+import java.io.IOException;
+
 public class GestorOrdenes {
 
     private ListaOrdenes listaOrdenes;
 
-    public GestorOrdenes(ListaOrdenes listaOrdenes) {
-        this.listaOrdenes = listaOrdenes;
+    public GestorOrdenes() throws IOException {
+        listaOrdenes = new ListaOrdenes();
     }
 
     public Orden generarOrden(Pago pago, Carrito carrito){
@@ -18,15 +20,21 @@ public class GestorOrdenes {
         return nuevaOrden;
     }
 
-    public boolean agregarOrden(Pago pago, Carrito carrito){
-        return listaOrdenes.agregarOrden( generarOrden(pago, carrito) );
+    public boolean agregarOrden(Pago pago, Carrito carrito) throws IOException {
+        boolean x = listaOrdenes.agregarOrden( generarOrden(pago, carrito) );
+        if (x)
+            listaOrdenes.guardarOrdenes();
+        return x;
     }
 
-    public boolean eliminarOrden(int id){
-        return listaOrdenes.borrarOrden(id);
+    public boolean eliminarOrden(int id) throws IOException {
+        boolean x = listaOrdenes.borrarOrden(id);
+        if (x)
+            listaOrdenes.guardarOrdenes();
+        return x;
     }
 
-    public boolean editarOrden(Pago pago, Carrito carrito, int id) {
+    public boolean editarOrden(Pago pago, Carrito carrito, int id) throws IOException {
         boolean regresar = false;
         Orden aEditar = listaOrdenes.getOrden(id);
 
@@ -35,12 +43,34 @@ public class GestorOrdenes {
             aEditar.setCarrito(carrito);
             aEditar.setPago(pago);
             regresar = listaOrdenes.agregarOrden(aEditar);
+            listaOrdenes.guardarOrdenes();
         }
         return regresar;
     }
 
+    public boolean actualizarEstado(int id, String estado) throws IOException {
+        Orden orden = listaOrdenes.buscarOrden(id);
+        if (orden != null ){
+            listaOrdenes.borrarOrden(id);
+            orden.setEstadoActual(estado);
+            listaOrdenes.agregarOrden(orden);
+            listaOrdenes.guardarOrdenes();
+            return true;
+        }
+        return false;
+    }
+
+    public Orden verOrden(int id){
+        return listaOrdenes.buscarOrden(id);
+    }
+
+    public String consultarEstado(int id){
+        Orden orden = listaOrdenes.buscarOrden(id);
+        return orden.getEstadoActual();
+    }
+
     public String verOrdenes(){
-        return listaOrdenes.toString();
+        return listaOrdenes.verOdernes();
     }
 
 }
