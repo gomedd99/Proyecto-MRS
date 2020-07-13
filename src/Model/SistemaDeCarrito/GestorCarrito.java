b68086824041ccf744d422e9be43fbbfbca78b7f
pandyshwallee <andreaar1605@gmail.com>
2020-07-12 20:59:33 -0500

    GestorCarrito: se apregan platillos de uno en uno
    Se reemplazo el metodo agregarPlatillos por agregarPlatillo.

--- src/Model/SistemaDeCarrito/GestorCarrito.java
+++ src/Model/SistemaDeCarrito/GestorCarrito.java
@@ -9,24 +9,7 @@ public class GestorCarrito {
 
     public GestorCarrito() {
         // inicia el carrito en null
-        carrito = null;
-    }
-
-    public Carrito generarCarrito(ArrayList<Platillo> platillos){
-        // Crea un carrito nuevo en this.carrito
-        // Se puede usar para vaciar el carrito
-        carrito = new Carrito(platillos);
-        return carrito;
-    }
-
-    public boolean agregarPatillos(ArrayList<Platillo> platillos) {
-        // Agrega una lista de platillos al carrito
-        boolean regresar = true;
-        for (int i=0;i<platillos.size();i++) {
-            Platillo p = platillos.get(i);
-            regresar = carrito.agregarPlatillos(p);
-        }
-        return regresar;
+        carrito = new Carrito();
     }
 
     public boolean agregarPatillo(Platillo platillo) {
